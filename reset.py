d0612505d2f4d73f9f7cc14e64590771f8ab738c
OstrichBanger <jonathanost@gmail.com>
2014-02-04 10:39:31 -0500

    added reset

---
+++ reset.py
@@ -0,0 +1,13 @@
+from app import app, models, db
+
+u = models.User.query.all()
+
+for u in users:
+	if u.username=='testing314'
+		db.session.delete(u)
+
+data = models.Datapoint.query.all()
+for d in data:
+	db.session.delete(d)
+
+db.session.commit()
