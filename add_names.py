9bcbff517671f19a84dc94f6e89d37963b2896bd
OstrichBanger <jonathanost@gmail.com>
2014-02-05 20:46:20 -0500

    first names

--- add_names.py
+++ add_names.py
@@ -4,8 +4,8 @@ users = models.User.query.all()
 names = ['Jon','Shivam','Harnsowl','Mitchell','Anthony','Kenny','Christian','Marcello','Sharang','Malcolm','Vasily','Ethan','Dan']
 i=0
 for u in users:
-	i=models.FirstName(name=names[i], author=u)
-	db.session.add(i)
+	a=models.FirstName(name=names[i], author=u)
+	db.session.add(a)
 	i=i+1
 
 db.session.commit()
